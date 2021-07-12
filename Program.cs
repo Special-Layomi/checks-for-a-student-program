b01f43d4525dd02be24dd84237f0e1b6ac00f6f1
Special-Layomi <87333581+Special-Layomi@users.noreply.github.com>
2021-07-12 19:55:15 +0100

    Add files via upload
    nested if else statement for checking the status of a part time or full time student

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+/* AKINOLA TITILAYO DEBORAH
+EES/18/19/0121
+300*/
+
+namespace Layo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Are you a full-time or part-time student?");
+            string student  = Convert.ToString(Console.ReadLine());
+            if (student == "full-time")
+            {
+                System.Console.WriteLine("What is your course of study?");
+                string course = Convert.ToString(Console.ReadLine());
+                System.Console.WriteLine(course + " is a good course");
+            }
+            else if  (student == "part-time")
+            {
+                System.Console.WriteLine("How many credits are you taking?");
+                int credit = Convert.ToInt32(Console.ReadLine());
+
+                if (credit > 6)
+                {
+                    System.Console.WriteLine("Too much units for a student");
+                }
+                else if (credit <= 6)
+                { 
+                    System.Console.WriteLine("Proceed to screen");
+                }
+                
+                
+
+            }
+        }
+    }
+}
